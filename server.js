fefd5fd8298c1b0fd80b2f87402b1d2760cff8f6
Anjo Tadena <anjo@smartstart.us>
2021-07-23 01:07:39 +0800

    Setup express to host our app in heroku

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const DIST_DIR = './dist/tulgy-zip';
+
+app.use(function(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+
+    next();
+});
+
+app.use(express.static(DIST_DIR));
+
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', { root: `${DIST_DIR}/` });
+});
+
+app.listen(process.env.PORT || 8080);
