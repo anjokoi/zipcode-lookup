6c458e2b86027bed1106198a7c07a8f59a47a7b1
Anjo Tadena <anjo@smartstart.us>
2021-07-27 06:20:07 +0800

    Add copy paste clipboard function

--- src/app/app.constant.ts
+++ src/app/app.constant.ts
@@ -1,3 +1,4 @@
 export const APP_TITLE = 'APP TITLE';
 
-export const ZIP_CODE_PATTERN = /(^\d{5}$)|(^\d{9}$)|(^\d{5}-\d{4}$)/;
+// export const ZIP_CODE_PATTERN_COMMA_SP = /(^\d{5}$)|(^\d{9}$)|(^\d{5}-\d{4}$)/;
+export const ZIP_CODE_PATTERN = /(^\d{5}(, +(\d{5}|\d{9}|\d{5}-\d{4}))*$)|(^\d{9}(, +(\d{5}|\d{9}|\d{5}-\d{4}))*$)|(^\d{5}-\d{4}(, +(\d{5}|\d{9}|\d{5}-\d{4}))*$)/;
--- src/app/page/home/components/search/search.component.ts
+++ src/app/page/home/components/search/search.component.ts
@@ -63,33 +63,34 @@ export class SearchComponent implements OnInit {
 
     // flag to disable button when searching
     this.searching = true;
+    setTimeout(() => {
+      // Collect all zip code subscriptions
+      this.observableCollections = of(this.zipCode.map(c => this.zipService.getZipCode(c)));
 
-    // Collect all zip code subscriptions
-    this.observableCollections = of(this.zipCode.map(c => this.zipService.getZipCode(c)));
+      // Store subscriptions to subscriptions attribute to unsubscribe later
+      this.subscriptions.push(
+        this.observableCollections
+          .pipe(mergeMap((v) => combineLatest(v)))
+          .subscribe((response) => {
+            this.searching = false;
 
-    // Store subscriptions to subscriptions attribute to unsubscribe later
-    this.subscriptions.push(
-      this.observableCollections
-        .pipe(mergeMap((v) => combineLatest(v)))
-        .subscribe((response) => {
-          this.searching = false;
+            if (response instanceof HttpErrorResponse || !Array.isArray(response)) {
+              return;
+            }
 
-          if (response instanceof HttpErrorResponse || !Array.isArray(response)) {
-            return;
-          }
-
-          this.zipCodes = response.filter(r => !!r).map((r: any) => ({
-              country: r.country,
-              postCode: r["post code"],
-              countryAbbreviation: r["country abbreviation"],
-              places: r.places.map((p: any) => ({ placeName: p["place name"], longitude: p.longitude, latitude: p.latitude, state: p.state, stateAbbreviation: p["state abbreviation"] }) as Place)
-            } as ZipCode))
-            .sort((a: ZipCode, b: ZipCode) => a.postCode.localeCompare(b.postCode));
-        }, error => {
-          console.error(error);
-          this.searching = false;
-        })
-    );
+            this.zipCodes = response.filter(r => !!r).map((r: any) => ({
+                country: r.country,
+                postCode: r["post code"],
+                countryAbbreviation: r["country abbreviation"],
+                places: r.places.map((p: any) => ({ placeName: p["place name"], longitude: p.longitude, latitude: p.latitude, state: p.state, stateAbbreviation: p["state abbreviation"] }) as Place)
+              } as ZipCode))
+              .sort((a: ZipCode, b: ZipCode) => a.postCode.localeCompare(b.postCode));
+          }, error => {
+            console.error(error);
+            this.searching = false;
+          })
+      );
+    }, 3000); 
   }
 
   add(event: MatChipInputEvent): void {
@@ -110,6 +111,43 @@ export class SearchComponent implements OnInit {
 
     if (index >= 0) {
       this.zipCode.splice(index, 1);
+      
+      this.zipCodeFormControl.setErrors(null);
+      this.zipCode.forEach((value) => {
+        if(value.trim() && this.checkIsZipCodeInValid(value.trim())) {
+          this.zipCodeFormControl.setErrors({ pattern: 'Invalid' })
+        }
+      })
     }
+  }
+
+  paste(event: ClipboardEvent): void {
+    console.log('xxx', event);
+    event.preventDefault(); //Prevents the default action
+
+    if (!event.clipboardData) {
+      return;
+    }
+
+    event.clipboardData
+      .getData('Text') // Gets the text pasted
+      .split(/;|,|\n/) // Splits it when a SEMICOLON or COMMA or NEWLINE
+      .forEach((value) => {
+        if(value.trim()){
+          this.zipCode.push(value.trim()); //Push if valid
+
+          if (this.checkIsZipCodeInValid(value.trim())) {
+            this.zipCodeFormControl.setErrors({
+              pattern: 'Invalid'
+            })
+          }
+        }
+      })
+  }
+
+  checkIsZipCodeInValid(value: string) {
+    const exp = new RegExp(/(^\d{5}(, +\d{5})*$)|(^\d{9}$)|(^\d{5}-\d{4}$)/);
+
+    return !exp.test(value);
   }
 }
